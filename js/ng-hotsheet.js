df7d3b7701dbb15b701b3d3e7175eeae24c0c1c2
Ryan Boone <falldowngoboone@gmail.com>
2014-11-07 17:37:20 -0600

    Added edit feature to Today items.

--- js/ng-hotsheet.js
+++ js/ng-hotsheet.js
@@ -10,4 +10,15 @@ hotsheet.controller('hotsheetCtrl', ['$scope', '$http', function($scope, $http){
   var date = new Date();
   $scope.date = date.toDateString();
 
+  $scope.edit = function(context) {
+    if (!context.edit)
+      context.edit = true;
+    else
+      context.edit = false;
+  };
+
+  $scope.save = function(context) {
+    context.edit = false;
+  };
+
 }]);
