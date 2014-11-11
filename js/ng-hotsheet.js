cd654fa3f10fb78bacfb89979292ab43ae114c78
Ryan Boone <falldowngoboone@gmail.com>
2014-11-11 14:15:18 -0600

    Added delete client functionality.

--- js/ng-hotsheet.js
+++ js/ng-hotsheet.js
@@ -1,6 +1,6 @@
 var hotsheet = angular.module('hotsheet',[]);
 
-hotsheet.controller('hotsheetCtrl', ['$scope', '$http', function($scope, $http){
+hotsheet.controller('hotsheetCtrl', ['$scope', '$http', '$window', function($scope, $http, $window){
 
   $http.get('/data/hotsheet.json')
     .success(function(data){
@@ -15,6 +15,22 @@ hotsheet.controller('hotsheetCtrl', ['$scope', '$http', function($scope, $http){
       delete context.edit;
     } else {
       context.edit = true;
+    }
+  };
+
+  $scope.addToday = function(client) {
+    client.today.unshift({item:'new'});
+  };
+
+  $scope.deleteClient = function(client) {
+    if ($window.confirm("Delete Client?")) {
+      var oldArr = $scope.clients;
+      $scope.clients = [];
+      oldArr.forEach(function(el){
+        if (el.name !== client.name) {
+          $scope.clients.push(el);
+        }
+      });
     }
   };
 
