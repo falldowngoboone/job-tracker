01b4145580ea2df1808c7c0f8a4661658efd57ac
Ryan Boone <falldowngoboone@gmail.com>
2014-11-19 14:12:15 -0600

    Broke out code to separate files, encapsulated in IIFEs.

---
+++ js/hotsheetServices.js
@@ -0,0 +1,21 @@
+(function(){
+  function dataServices($http){
+    var dataServices = {};
+
+    dataServices.get = function() {
+      return $http.get('/data/test.json');
+    };
+    dataServices.saveJSON = function(clients) {
+      this.clients = clients;
+      return function() {
+        var data = JSON.stringify(this.clients, null, 2);
+        $http.post('/scripts/writeJSON.php', data);
+      };
+    };
+
+    return dataServices;
+  }
+
+  angular.module('hotsheet')
+    .factory('dataServices', ['$http', dataServices]);
+}());
