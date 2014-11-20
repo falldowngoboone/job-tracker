4ff017324e36c567d27a9f806174ff2956cb297a
Ryan Boone <falldowngoboone@gmail.com>
2014-11-20 16:48:03 -0600

    Added routes for more encapsulation of code.

--- js/hotsheet.js
+++ js/hotsheet.js
@@ -1,3 +1,13 @@
 (function() {
-  angular.module('hotsheet',[]);
+  function config($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/views/main.html',
+        controller: 'hotsheetCtrl',
+        controllerAs: 'hotsheet'
+      });
+  }
+
+  angular.module('hotsheet',['ngRoute'])
+    .config(['$routeProvider', config]);
 }());
