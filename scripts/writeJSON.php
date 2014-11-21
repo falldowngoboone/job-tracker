60ede96506f8441ed1da12c7428ffc02379a2d80
Ryan Boone <falldowngoboone@gmail.com>
2014-11-21 09:34:38 -0600

    Now PHP script creates the data/ directory if none exists.

--- scripts/writeJSON.php
+++ scripts/writeJSON.php
@@ -1,7 +1,13 @@
 <?php
 
+function file_force_contents($file, $data) {
+  if(!is_dir("../data")) {
+    mkdir("../data");
+  }
+  file_put_contents($file, $data);
+}
 
 $file = "../data/hs-data.json";
 $data = file_get_contents('php://input');
 
-file_put_contents($file, $data);
+file_force_contents($file, $data);
