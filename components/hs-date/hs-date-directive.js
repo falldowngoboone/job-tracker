8daa08e49ec80adc6f69d34e8b482c704127155f
Ryan Boone <falldowngoboone@gmail.com>
2014-11-24 15:56:20 -0600

    Added default behavior, formatting logic, date logic and html templating to hsDate directive.

--- components/hs-date/hs-date-directive.js
+++ components/hs-date/hs-date-directive.js
@@ -1,35 +1,71 @@
+// hsDate directive
+// Format: <hs-date [date] [format]>[Template text]</hs-date>
+// Attributes:
+  // date (DEFAULT: current date) - accepts JavaScript appropriate format (currently UTC)
+  // format (DEFAULT: set in format variable below) - any accepted AngularJS date filter
+    // format (https://docs.angularjs.org/api/ng/filter/date)
+// Template:
+  // Accepts any text or html
+  // %date% - placeholder for specified date (default or date attribute)
+
+// Examples:
+
+  // No date attribute, default formatting
+  // <hs-date></hs-date>
+    // -> Outputs current date in 'MM/dd' format
+
+  // Specific date, default formatting
+  // <hs-date date="2002-11-23"></hs-date>
+    // -> 11/23
+
+  // Specific date, custom formatting
+  // <hs-date date="2002-11-23" format="fullDate"></hs-date>
+    // -> Saturday, November 23, 2002
+
+  // With templating
+  // <hs-date date="2002-11-23" format="fullDate">My wedding was: <time datetime="2002-11-23">%date%</time>.</hs-date>
+    // -> My wedding was: <time datetime="2002-11-23">Saturday, November 23, 2002</time>.
+
 (function(){
   'use strict';
 
   function hsDate(dateFilter) {
     function link(scope, element, attrs) {
-      var currentDate = new Date(),
-          localTime = currentDate.getTime(),
-          localOffset = currentDate.getTimezoneOffset() * 60 * 1000, // in milliseconds
-          format = 'MM/dd';
+      var today = new Date(),
+          format = attrs.format || 'MM/dd';
 
-      if (attrs.date) {
-        var date = new Date(attrs.date),
-            // convert the date attribute, which is GMT midnight, to local midnight
-            // or else it shows previous day
-            dateLocal = new Date(date.getTime() + localOffset);
+      var localizeDate = function(date) {
+        if (!date) {
+          return false;
+        } else {
+          var gmtDate = new Date(date),
+              localOffset = today.getTimezoneOffset() * 60 * 1000, // in milliseconds
+              localDate = new Date(gmtDate.getTime() + localOffset);
 
-        element.text(dateFilter(dateLocal, format));
-
-        if (dateLocal <= currentDate) {
-          element.addClass("late");
+          return localDate;
         }
-      } else {
-        element.text(dateFilter(currentDate, format));
+      };
+
+      var date = localizeDate(attrs.date) || today;
+
+      // If date is set and less than or equal to today, add late class
+      if (attrs.date && date <= today) {
+        element.addClass("late");
       }
+
+      // Templating with %date%
+      if (!element.text()) {
+        element.html(dateFilter(date, format));
+      } else {
+        // Template out hs-date with %date% placeholder in element text
+        var html = element.html().replace(/(\%date\%)/g, dateFilter(date, format));
+        element.html(html);
+      }
+
     }
 
     return {
       restrict: 'EA',
-      // scope: {
-      //   due: '='
-      // },
-      //templateUrl: 'components/hs-date/hs-date.html',
       link: link
     };
   }
