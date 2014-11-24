bccee6edb3db005e52c37c99233eb3c47af038ef
Ryan Boone <falldowngoboone@gmail.com>
2014-11-24 10:24:34 -0600

    Manipulating the DOM through the hs-date directive using link function.

--- components/hs-date/hs-date-directive.js
+++ components/hs-date/hs-date-directive.js
@@ -1,15 +1,22 @@
 (function(){
+  'use strict';
 
-  function hsDate() {
+  function hsDate(dateFilter) {
+    function link(scope, element, attrs) {
+      var currentDate = new Date();
+      element.text(dateFilter(currentDate, 'EEE, MMM d, yyyy'));
+    }
+
     return {
       restrict: 'E',
       scope: {
         due: '='
       },
-      templateUrl: 'components/hs-date/hs-date.html'
+      //templateUrl: 'components/hs-date/hs-date.html',
+      link: link
     };
   }
 
   angular.module('hotsheet.hsDate',[])
-    .directive('hsDate', hsDate);
+    .directive('hsDate', ['dateFilter', hsDate]);
 }());
