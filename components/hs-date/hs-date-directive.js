08badcada3eb7e1ab508692b7e375a9bdbcb6baa
Ryan Boone <falldowngoboone@gmail.com>
2014-11-24 12:36:24 -0600

    hsDate directive now operational.

--- components/hs-date/hs-date-directive.js
+++ components/hs-date/hs-date-directive.js
@@ -3,15 +3,32 @@
 
   function hsDate(dateFilter) {
     function link(scope, element, attrs) {
-      var currentDate = new Date();
-      element.text(dateFilter(currentDate, 'EEE, MMM d, yyyy'));
+      var currentDate = new Date(),
+          localTime = currentDate.getTime(),
+          localOffset = currentDate.getTimezoneOffset() * 60 * 1000, // in milliseconds
+          format = 'MM/dd';
+
+      if (attrs.date) {
+        var date = new Date(attrs.date),
+            // convert the date attribute, which is GMT midnight, to local midnight
+            // or else it shows previous day
+            dateLocal = new Date(date.getTime() + localOffset);
+
+        element.text(dateFilter(dateLocal, format));
+
+        if (dateLocal <= currentDate) {
+          element.addClass("late");
+        }
+      } else {
+        element.text(dateFilter(currentDate, format));
+      }
     }
 
     return {
-      restrict: 'E',
-      scope: {
-        due: '='
-      },
+      restrict: 'EA',
+      // scope: {
+      //   due: '='
+      // },
       //templateUrl: 'components/hs-date/hs-date.html',
       link: link
     };
