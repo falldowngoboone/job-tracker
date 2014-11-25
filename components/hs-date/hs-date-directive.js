03959dd9d72035e5c1dc1a9333e55a9dc767964a
Ryan Boone <falldowngoboone@gmail.com>
2014-11-25 12:44:26 -0600

    Squashed the hsDate update bug. Removed %date% templating, which was the cause. Templating now done in format attribute, html not allowed.

--- components/hs-date/hs-date-directive.js
+++ components/hs-date/hs-date-directive.js
@@ -1,12 +1,9 @@
 // hsDate directive
-// Format: <hs-date [date] [format]>[Template text]</hs-date>
+// Format: <hs-date [date] [format]></hs-date>
 // Attributes:
-  // date (DEFAULT: current date) - accepts JavaScript appropriate format (currently UTC)
+  // date (DEFAULT: current date) - accepts any JavaScript appropriate format
   // format (DEFAULT: set in format variable below) - any accepted AngularJS date filter
     // format (https://docs.angularjs.org/api/ng/filter/date)
-// Template:
-  // Accepts any text or html
-  // %date% - placeholder for specified date (default or date attribute)
 
 // Examples:
 
@@ -22,25 +19,31 @@
   // <hs-date date="2002-11-23" format="fullDate"></hs-date>
     // -> Saturday, November 23, 2002
 
-  // With templating
-  // <hs-date date="2002-11-23" format="fullDate">My wedding was: <time datetime="2002-11-23">%date%</time>.</hs-date>
-    // -> My wedding was: <time datetime="2002-11-23">Saturday, November 23, 2002</time>.
+  // Templating the format
+  // <hs-date date="2002-11-23" format="(ATV MM/dd)"></hs-date>
+    // -> (ATV ll/23)
+
 
 (function(){
   'use strict';
 
   function hsDate(dateFilter) {
     function link(scope, element, attrs) {
-      var today = new Date(),
-          format = attrs.format || 'MM/dd';
 
+      // This function transforms a date built in UTC with no specified time
+      // to display at the locale of the browser viewing it. Not very good
+      // for international applications. Perhaps specifying a time is a better
+      // solution.
       var localizeDate = function(date) {
+        var today = new Date();
+
         if (!date) {
           return false;
         } else {
           var gmtDate = new Date(date);
 
           if (isNaN(gmtDate.getTime())){
+            console.log("Value " + date + " is not a valid date.");
             return "string";
           } else {
             var localOffset = today.getTimezoneOffset() * 60 * 1000, // in milliseconds
@@ -51,32 +54,27 @@
         }
       };
 
-      // Wrap in function parseDate(val) that parses the date out correctly
-      // Observe changes on the date attribute
-        // then call parseDate(currentVal) on change
+      var setDate = function(val) {
+        var today = new Date(),
+            date = localizeDate(val) || today,
+            format = attrs.format || 'MM/dd';
 
-      var date = localizeDate(attrs.date) || today;
-
-
-      // If date is set and less than or equal to today, add late class
-      if (attrs.date && date <= today) {
-        element.addClass("late");
-      }
-
-      // Templating with %date%
-      if (!element.text() && date !== "string") {
-        element.html(dateFilter(date, format));
-      } else {
-        // Template out hs-date with %date% placeholder in element text
-        var replaceText;
         if (date === "string") {
-          replaceText = attrs.date;
+          element.text(val);
         } else {
-          replaceText = dateFilter(date, format);
+          element.text(dateFilter(date, format));
         }
-        var html = element.html().replace(/(\%date\%)/g, replaceText);
-        element.html(html);
-      }
+        // If date is set and less than or equal to today, add late class
+        if (val && date <= today) {
+          element.addClass("late");
+        }
+
+      };
+
+      // setDate(attrs.date);
+      attrs.$observe('date', function(v){
+        setDate(v);
+      });
 
     }
 
